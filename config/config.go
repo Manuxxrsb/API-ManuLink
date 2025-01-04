33275a531cd6cac26fab4756ac7fb3c3a22a1d20
Manuxxrsb <manuxxrsb@gmail.com>
2025-01-04 12:34:23 -0300

    Elimina los registros de log de las variables de entorno en la función DBURL

--- config/config.go
+++ config/config.go
@@ -2,18 +2,11 @@ package config
 
 import (
 	"fmt"
-	"log"
 	"os"
 )
 
 // DBURL genera la URL de conexión a la base de datos PostgreSQL
 func DBURL() string {
-
-	log.Println("HOST:", os.Getenv("PGHost"))
-	log.Println("USER:", os.Getenv("PGUser"))
-	log.Println("NAME:", os.Getenv("PGName"))
-	log.Println("PORT:", os.Getenv("PGPort"))
-
 
 	DBHost := os.Getenv("PGHost")
 	DBUser := os.Getenv("PGUser")
