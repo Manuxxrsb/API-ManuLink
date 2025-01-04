236aad853576fafbec606b9e3c47a37ab2c90965
Manuxxrsb <manuxxrsb@gmail.com>
2025-01-04 12:01:06 -0300

    Carga el archivo .env y maneja errores en la función DBURL

--- config/config.go
+++ config/config.go
@@ -3,11 +3,17 @@ package config
 import (
 	"fmt"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 // DBURL genera la URL de conexión a la base de datos PostgreSQL
 func DBURL() string {
-	
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+
 	DBHost := os.Getenv("DBHost")
 	DBUser := os.Getenv("DBUser")
 	DBPassword := os.Getenv("DBPassword")
